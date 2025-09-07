1bc3eef3130aab735497fdb0363f6acdadfbe389
Giessen <3705150+Giessen@users.noreply.github.com>
2025-09-07 19:43:13 +0800

    smart Esc

--- internal/display/infowindow.go
+++ internal/display/infowindow.go
@@ -181,7 +181,8 @@ func (i *InfoWindow) displayBuffer() {
 //var keydisplay = []string{"^Q Quit, ^S Save, ^O Open, ^G Help, ^E Command Bar, ^K Cut Line", "^F Find, ^Z Undo, ^Y Redo, ^A Select All, ^D Duplicate Line, ^T New Tab"}
 //@ ^D Del Line
 var keydisplay = []string{"^X Quit, ^S Save, ^O Open, ^T New Tab,      F5/6 CMD/SH, ^G/D Goto/Del Line, ^K/C|J Cut/Copy",
-                          "^F Find, ^Z Undo, ^Y Redo, ^E(A) Mark(All), ^/ Comment,  ^I Find Text,       ^V|U Paste"}
+                          "^F Find, ^Z Undo, ^Y Redo, ^E(A) Mark(All), ^/ Comment,  ^I Find Text,       ^V|U Paste",
+                          "ESC Exit key/help menus; Exit mark selection; Show path"}
 
 func (i *InfoWindow) displayKeyMenu() {
 	// TODO: maybe make this based on the actual keybindings
