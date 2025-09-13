ba063bd4db2a81aaad4d254bd070b96c298b71b9
Giessen <3705150+Giessen@users.noreply.github.com>
2025-09-14 04:23:56 +0800

    ctrlH search and replace

--- internal/display/infowindow.go
+++ internal/display/infowindow.go
@@ -180,9 +180,9 @@ func (i *InfoWindow) displayBuffer() {
 
 //var keydisplay = []string{"^Q Quit, ^S Save, ^O Open, ^G Help, ^E Command Bar, ^K Cut Line", "^F Find, ^Z Undo, ^Y Redo, ^A Select All, ^D Duplicate Line, ^T New Tab"}
 //@ ^D Del Line
-var keydisplay = []string{"^X Quit, ^S Save, ^O Open, ^T New Tab,      F5/6 CMD/SH, ^G/D Goto/Del Line, ^K/C|J Cut/Copy",
-                          "^F Find, ^Z Undo, ^Y Redo, ^W(A) Mark(All), ^/ Comment,  ^I Find Text,       ^V|U Paste",
-                          " ESC Exit Key,Help Menu/Termpane/Mark Select; Show Path  ^L Line Number      ^/M] JumpTo/SelectTo Matching Bracket"}
+var keydisplay = []string{"^X   Quit,         ^S Save, ^O Open, ^T    New Tab,    F5/6 CMD/SH,  ^G/D Goto/Del Line, ^K/C|J Cut/Copy",
+                          "^F/H Find/Replace, ^Z Undo, ^Y Redo, ^W(A) Mark(All), ^/    Comment, ^I   Find Text,     ^V|U   Paste",
+                          " ESC  Exit Menu/Termpane/Mark Selection; Show Path    ^L    Line #   ^/M] JumpTo/SelectTo Matching Bracket"}
 
 func (i *InfoWindow) displayKeyMenu() {
 	// TODO: maybe make this based on the actual keybindings
